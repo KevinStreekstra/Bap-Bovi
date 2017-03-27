132b55c1ece718597e1baeb64c6c410125c40081
KevinStreekstra <kevinstreekstra07@gmail.com>
2017-03-27 10:59:51 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,35 @@
-<?php 
-	// maak dit scipt werkend: zoek de fouten	
-	
-	include 'from.php';
-	
-	if (isset($_POST['sendcontact']))
-	{
-		include 'response.php';
-	}
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>Mijn website - </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<?php
+
+
+// Settings
+include 'includes/config.php';
+
+// head section <head>
+include 'views/head.php';
+
+// header section <header>
+include 'views/header.php';
+
+// menu balk <nav>
+include 'views/menu.php';
+
+// content home page
+include 'views/home.php';
+
+// Here comes the footer
+include 'views/footer.php';
+
+?>
+
